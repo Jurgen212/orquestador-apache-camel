95da257a6cc2898abe4c8c7a8ab102eb00898d3b
Jurgen Sanclemente <jurgensanclemente@gmail.com>
2024-04-09 19:33:24 -0500

    Orquestador

---
+++ OrquestadorEnigma/src/main/java/co/com/sanclemente/microservice/resolveEnigmaApi/api/GetStepApi.java
@@ -0,0 +1,15 @@
+package co.com.sanclemente.microservice.resolveEnigmaApi.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import co.com.sanclemente.microservice.resolveEnigmaApi.model.JsonApiBodyRequest;
+
+import javax.validation.Valid;
+
+public interface GetStepApi {
+
+    @PostMapping("/getSteps")
+    ResponseEntity<?> getStepsPost(@Valid @RequestBody JsonApiBodyRequest body);
+}
